ab3a1c20df5eb7ed5d240d7a6360609c13bcbfff
linboj <spps2215@gmail.com>
2021-06-15 12:24:16 +0800

    hack4 ver1

--- hack4/frontend/src/graphql/queries.js
+++ hack4/frontend/src/graphql/queries.js
@@ -2,7 +2,7 @@ import { gql } from "@apollo/client"
 
 export const STATSCOUNT_QUERY=gql`
     query statsCount(
-        $severity: Int!
+        $severity: Int
         $locationKeywords: [String!]
         ){
             statsCount(severity:$severity,locationKeywords:$locationKeywords)
